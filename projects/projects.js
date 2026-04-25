e99663e431606032e3f39ef3949fcfab3bbed99a
Zachary Louie <zacharylouie@Zacharys-MacBook.local>
2026-04-24 23:02:15 -0700

    project counter for the projects page

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,6 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `${projects.length} Projects`;
