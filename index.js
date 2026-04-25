e1d2f632da83b7f92f5895688c809b5d44ae314f
Zachary Louie <zacharylouie@Zacharys-MacBook.local>
2026-04-24 23:05:23 -0700

    display 3 latest projects on the homepage

---
+++ index.js
@@ -0,0 +1,8 @@
+import { fetchJSON, renderProjects } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(latestProjects, projectsContainer, 'h2');
